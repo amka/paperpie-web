54935391839d9ca44cffdb479cbd6d719f00b288
Andrey Maksimov <andrey@tenderowl.com>
2024-01-17 18:35:10 +0300

    Set default value for a notes with empty titles

--- src/components/SidebarDrawer.vue
+++ src/components/SidebarDrawer.vue
@@ -5,31 +5,26 @@
   >
     <v-list :selected="[notesStore.currentNote?.noteId]" class="px-4">
       <v-list-subheader>Notes</v-list-subheader>
-      <template v-if="notesStore.notes != undefined">
-        <template v-for="note in notesStore.notes" :key="note.noteId">
-          <v-list-item
-            :value="note.noteId"
-            @click="notesStore.currentNote = note"
-            :title="note.title"
-            color="primary"
-            rounded="rounded"
-            density="compact"
-            nav
-          >
-          </v-list-item>
-        </template>
-      </template>
-      <template v-else>
-        <v-progress-circular />
+      <template v-for="note in notesStore.notes" :key="note.noteId">
+        <v-list-item
+          :title="note.title!.length > 0 ? note.title  : 'Untitled Note'"
+          :value="note.noteId"
+          color="primary"
+          density="compact"
+          nav
+          rounded="rounded"
+          @click="notesStore.currentNote = note"
+        >
+        </v-list-item>
       </template>
     </v-list>
   </v-navigation-drawer>
 </template>
 
-<script setup lang="ts">
-import { useNotesStore } from "@/store/notes";
-import { onMounted } from "vue";
-import { useAppStore } from "@/store/app";
+<script lang="ts" setup>
+import {useNotesStore} from "@/store/notes";
+import {onMounted} from "vue";
+import {useAppStore} from "@/store/app";
 
 const appState = useAppStore();
 const notesStore = useNotesStore();
